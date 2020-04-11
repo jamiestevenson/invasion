dc9130e59d8e79a01dec08db7721615a1073e33b
Jamie Stevenson <falteringlemniscate@gmail.com>
2020-04-11 19:20:45 +0100

    adding location upgrades to types

--- invasion-app/src/invasion/types.ts
+++ invasion-app/src/invasion/types.ts
@@ -185,7 +185,17 @@ const consequencesDeck : Consequence[] = [
       "TRIGGER: The perimeter tile with the token is invaded",
       "EFFECT: Gain one TECH"
     ],
-    quote: "Perhaps you shoudl have studied harder."
+    quote: "Perhaps you should have studied harder."
+  },
+  {
+    title: "Influential Collaborators",
+    effects: [
+      "PLACE: A 'Collaborators' token on a perimeter tile",
+      "TRIGGER: Spend one drone at this location to move the Collaborators token to a perimeter tile",
+      "EFFECT: When this perimeter tile is invaded, reduce the panic level by the population of this tile"
+    ],
+    quote: "They will be last.",
+    note: "Token remains on perimeter tile after it is invaded"
   },
   {
     title: "Resistance",
@@ -252,6 +262,239 @@ const consequencesDeck : Consequence[] = [
     effects: ["Place a resistance token from N-N-W"]
   }
 ];
+
+const buildings : Building = [
+  {
+    name: "Relay",
+    drones: 2,
+    requirement: {
+      power: 1
+    },
+    effects: ["Place +1 POWER token on an adjacent location"],
+    upgrades: [
+      {
+        requirement: {
+          power: 2
+        },
+        effects: ["Broadcast Power: Drones move out of this location for no movement cost"]
+      },
+      {
+        requirements: {
+          power: 1,
+          materials: 1
+        },
+        effects: ["Hazard: Reduce resistance stacks that enter this location by one"]
+      }
+    ]
+  },
+  {
+    name: "Conveyer",
+    drones: 2,
+    requirement: {
+      material: 1
+    },
+    effects: ["Place +1 MATERIAL token on an adjacent location"],
+    upgrades: [
+      {
+        requirement: {
+          material: 3
+        },
+        effects: ["Place +1 MATERIAL token on an adjacent location"]
+      },
+      {
+        requirements: {
+          population: 1,
+          materials: 1
+        },
+        effects: ["Hazard: Reduce resistance stacks that enter this location by one"]
+      }
+    ]
+  },
+  {
+    name: "Drone Hive",
+    drones: 3,
+    requirement: {
+      population: 1
+    },
+    effects: ["One drone spawns here on the invader's turn"],
+    upgrades: [
+      {
+        requirement: {
+          material: 1,
+          power: 1,
+          population: 1
+        },
+        effects: ["One additional drone spawns here on the invader's turn"]
+      }
+    ]
+  },
+  {
+    name: "Pod Farm",
+    drones: 3,
+    requirement: {
+      population: 2
+    },
+    effects: ["Gain one TECHNOLOGY"],
+    upgrades: [
+      {
+        requirement: {
+          population: 2,
+          power: 1
+        },
+        effects: ["One additional drone spawns here on the invader's turn"]
+      },
+      {
+        requirement: {
+          population: 3
+        },
+        effects: ["Gain one additional POD SOLDIER"]
+      }
+    ]
+  },
+  {
+    name: "Laboratory",
+    drones: 3,
+    requirement: {
+      power: 2,
+      materials: 2
+    },
+    effects: [
+      "Gain one TECHNOLOGY",
+      "TRIGGER: Consume four drones at this location. EFFECT: Gain a TECH",
+      "TRIGGER: Consume drone at this location to CYCLE a building of the same cost or lower",
+      "SYNERGY: Pylon exists. EFFECT: -1 TECH cost",
+      "SYNERGY: Skybeam exists. EFFECT: -1 TECH cost"
+    ],
+    upgrades: []
+  },
+  {
+    name: "Skybeam Array",
+    drones: 3,
+    requirement: {
+      materials: 1,
+      power: 2
+    },
+    effects: [
+      "Increase ALERT level (when first placed)"
+    ],
+    upgrades: [
+      {
+        requirement:{
+          materials: 1,
+          power: 3,
+        },
+        effects: [
+          "TRIGGER: Start of Invader's turn. EFFECT: Place a 'Teleport Drone' token on this location",
+          "TRIGGER: Use a 'Teleport Drone' token. EFFECT: Move a drone from any invaded tile to any invaded tile"
+        ]
+      },
+      {
+        requirement:{
+          materials: 1,
+          power: 2,
+          population: 2
+        },
+        effects: [
+          "EFFECT: Reduce alert level by 2"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Fabricator",
+    drones: 4,
+    requirement: {
+      materials: 1,
+      power: 1,
+      population: 1
+    },
+    effects: [
+      "Gain one TECH",
+      "TRIGGER: Begin invader's turn. EFFECT: Two drones spawn at this location"
+    ],
+    upgrades: [
+      {
+        requirement:{
+          materials: 1,
+          power: 2,
+          population: 1,
+        },
+        effects: [
+          "EFFECT: Drones leave this location for no mevement cost"
+        ]
+      },
+      {
+        requirement:{
+          materials: 2,
+          power: 1,
+          population: 2
+        },
+        effects: [
+          "EFFECT: Reduce alert level by 2"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Pylon",
+    drones: 5,
+    requirement: {
+      materials: 2,
+      power: 2
+    },
+    effects: [
+      "Gain one TECH",
+      "Counts for all purposes as a second HQ",
+      "Drones move between HQs as if they are adjacent"
+    ],
+    upgrades: [
+      {
+        requirement:{
+          materials: 2,
+          power: 2,
+          population: 1
+        },
+        effects: [
+          "TRIGGER: Spend one drone at this location. EFFECT: Reduce panic level at this location by its population"
+        ]
+      }
+    ]
+  },
+  {
+    name: "Death Ray",
+    drones: 5,
+    requirement: {
+      materials: 1,
+      power: 2
+    },
+    effects: [
+      "TRIGGER: Spend one drone at this location. EFFECT: In adjacent location EITHER reduce all resistance stacks by one OR reduce one resistance stack by this location's POWER",
+      "Counts for all purposes as a second HQ",
+      "Drones move between HQs as if they are adjacent"
+    ],
+    upgrades: [
+      {
+        requirement:{
+          materials: 1,
+          power: 3
+        },
+        effects: [
+          "Range increases to two (i.e. adjacent to adjacent)"
+        ]
+      },
+      {
+        requirement:{
+          materials: 1,
+          power: 2,
+          population: 1,
+        },
+        effects: [
+          "You may activate this location after resistance units have moved."
+        ]
+      }
+    ]
+  }
+]
 
 const hqTile: Tile = {
   terrain: {
@@ -322,4 +565,17 @@ export interface Consequence {
   effects: string[];
   quote?: string;
   note?: string;
+}
+
+export interface Building {
+  name: string,
+  drones: number,
+  requirement: Resources,
+  effect: string[]
+}
+
+export interface Resources {
+  power: number,
+  material: number,
+  population: number
 }
